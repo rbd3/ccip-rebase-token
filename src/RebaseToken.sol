fa5ca0615cd130d655844a62a29a5e9de55c432d
rbd3 <rabedesanaandry01@gmail.com>
2025-04-11 16:16:28 +0300

    Add access control

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.28;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
 /**
  * @title RebaseToken
@@ -10,7 +12,7 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  * @notice The interest rate in the smart contract can only decrease
  * @notice Each user will have their own interest rate that is the global interest rate at the time of deposing
  */
-contract RebaseToken is ERC20 {
+contract RebaseToken is ERC20, Ownable, AccessControl {
     /*error*/
     error RebaseToken__InterestRateCanOnlyDecrease(
         uint256 s_interestRate,
@@ -18,6 +20,7 @@ contract RebaseToken is ERC20 {
     );
 
     uint256 private constant PRECISION_FACTOR = 1e18;
+    bytes32 private constant MINT_BURN_ROLE = keccak256("MINT_BURN_ROLE");
     uint256 private s_interestRate = 5e10;
     mapping(address => uint256) private s_userInterestRate;
     mapping(address => uint256) private s_userLastUpdatedTimeStamp;
@@ -25,14 +28,19 @@ contract RebaseToken is ERC20 {
     /*event*/
     event InterestRareSet(uint256 newInterestRate);
 
-    constructor() ERC20("Rebase Token", "RBT") {}
+    constructor() ERC20("Rebase Token", "RBT") Ownable(msg.sender) {}
+
+    function mintAndBurnRole(address _account) external onlyOwner {
+        _grantRole(MINT_BURN_ROLE, _account);
+    }
 
     /**
      * @notice Set the interest Rate in the contract
      * @param _newInterestRate the new interest rate to set
      * @dev the new interest rate can only decrease
      */
-    function setInterestRate(uint256 _newInterestRate) external {
+
+    function setInterestRate(uint256 _newInterestRate) external onlyOwner {
         if (_newInterestRate < s_interestRate) {
             revert RebaseToken__InterestRateCanOnlyDecrease(
                 s_interestRate,
@@ -43,7 +51,10 @@ contract RebaseToken is ERC20 {
         emit InterestRareSet(_newInterestRate);
     }
 
-    function mint(address _to, uint256 _amount) external {
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyRole(MINT_BURN_ROLE) {
         _mintAccruedInterest(_to);
         s_userInterestRate[_to] = s_interestRate;
         _mint(_to, _amount);
@@ -79,11 +90,55 @@ contract RebaseToken is ERC20 {
         _mint(_user, balanceIncrease);
     }
 
-    function burn(address _from, uint256 _amount) external {
+    function burn(
+        address _from,
+        uint256 _amount
+    ) external onlyRole(MINT_BURN_ROLE) {
         if (_amount == type(uint256).max) {
             _amount = balanceOf(_from);
         }
         _mintAccruedInterest(_from);
         _burn(_from, _amount);
+    }
+
+    function transfer(
+        address _recipient,
+        uint256 _amount
+    ) public override returns (bool) {
+        _mintAccruedInterest(msg.sender);
+        _mintAccruedInterest(_recipient);
+        if (_amount == type(uint256).max) {
+            _amount = balanceOf(msg.sender);
+        }
+        if (balanceOf(_recipient) == 0) {
+            s_userInterestRate[_recipient] = s_userInterestRate[msg.sender];
+        }
+        return super.transfer(_recipient, _amount);
+    }
+
+    function transferFrom(
+        address _sender,
+        address _recipient,
+        uint256 _amount
+    ) public override returns (bool) {
+        _mintAccruedInterest(_sender);
+        _mintAccruedInterest(_recipient);
+        if (_amount == type(uint256).max) {
+            _amount = balanceOf(_sender);
+        }
+        if (balanceOf(_recipient) == 0) {
+            s_userInterestRate[_recipient] = s_userInterestRate[_sender];
+        }
+        return super.transferFrom(_sender, _recipient, _amount);
+    }
+
+    function principaleBalanceOf(
+        address _user
+    ) external view returns (uint256) {
+        return super.balanceOf(_user);
+    }
+
+    function getInterestRate() public view returns (uint256) {
+        return s_interestRate;
     }
 }
